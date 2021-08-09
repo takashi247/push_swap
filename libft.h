66b61f5694437401393ff3861513b47047b48549
takashi247 <takashi247@gmail.com>
2021-08-10 07:53:24 +0900

    add ft_isspace

--- libft.h
+++ libft.h
@@ -82,5 +82,6 @@ unsigned char	*ft_strjoin_u(unsigned char const *us1,
 void			ft_putstr_fd_u(unsigned char *us, int fd);
 void			ft_putendl_fd_u(unsigned char *us, int fd);
 int				ft_atoi_s(const char *str);
+int				ft_isspace(int c);
 
 #endif

2b879622cb4ce6e60ab22ecc81c2cc12f0d6f2fa
takashi247 <takashi247@gmail.com>
2021-09-11 16:16:01 +0900

    add basic header files

---
+++ include/push_swap.h
@@ -0,0 +1,14 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include "libft.h"
+# include "t_bool.h"
+
+// need to delete
+# include <stdio.h>
+
+# define ERR_MSG "Error\n"
+
+#endif

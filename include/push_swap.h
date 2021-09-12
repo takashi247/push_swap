d2422c7e1ba561a0a769cdb9d85640dabdcbee15
takashi247 <takashi247@gmail.com>
2021-09-12 09:54:06 +0900

    add header and bidirectional list structure

--- include/push_swap.h
+++ include/push_swap.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tnishina <tnishina@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/12 09:52:32 by tnishina          #+#    #+#             */
+/*   Updated: 2021/09/12 09:52:38 by tnishina         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
@@ -10,5 +22,15 @@
 # include <stdio.h>
 
 # define ERR_MSG "Error\n"
+
+typedef struct s_blist
+{
+	int				num;
+	struct s_blist	*prev;
+	struct s_blist	*next;
+}	t_blist;
+
+/* is_int_range.c */
+t_bool		ft_is_int_range(const char *str);
 
 #endif
